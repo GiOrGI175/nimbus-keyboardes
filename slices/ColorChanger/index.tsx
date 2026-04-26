c8678024ff947ceb227c947bc8b5610b81439bdd
GiOrGI175 <161075967+GiOrGI175@users.noreply.github.com>
2026-04-26 00:46:33 -0700

    feature/color changer section

--- slices/ColorChanger/index.tsx
+++ slices/ColorChanger/index.tsx
@@ -1,6 +1,54 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useCallback, useState } from 'react';
 import { Content } from '@prismicio/client';
-import { SliceComponentProps } from '@prismicio/react';
+import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
+import { Bounded } from '@/components/Bounded';
+import clsx from 'clsx';
+import Image from 'next/image';
+import { Canvas } from '@react-three/fiber';
+import Scene from './Scene';
+
+export const KEYCAP_TEXTURES = [
+  {
+    id: 'goodwell',
+    name: 'Goodwell',
+    path: '/goodwell_uv.png',
+    knobColor: '#E44E21',
+  },
+  {
+    id: 'dreamboard',
+    name: 'Dreamboard',
+    path: '/dreamboard_uv.png',
+    knobColor: '#E9759F',
+  },
+  {
+    id: 'cherrynavy',
+    name: 'Cherry Navy',
+    path: '/cherrynavy_uv.png',
+    knobColor: '#F06B7E',
+  },
+  {
+    id: 'kick',
+    name: 'Kick',
+    path: '/kick_uv.png',
+    knobColor: '#FD0A0A',
+  },
+  {
+    id: 'oldschool',
+    name: 'Old School',
+    path: '/oldschool_uv.png',
+    knobColor: '#B89D82',
+  },
+  {
+    id: 'candykeys',
+    name: 'Candy Keys',
+    path: '/candykeys_uv.png',
+    knobColor: '#F38785',
+  },
+];
+
+type keycapTexture = (typeof KEYCAP_TEXTURES)[number];
 
 /**
  * Props for `ColorChanger`.
@@ -11,15 +59,90 @@ export type ColorChangerProps = SliceComponentProps<Content.ColorChangerSlice>;
  * Component for "ColorChanger" Slices.
  */
 const ColorChanger: FC<ColorChangerProps> = ({ slice }) => {
+  const [selectedTextureId, setSelectedTextureId] = useState(
+    KEYCAP_TEXTURES[0].id,
+  );
+  const [backgroundText, setBackgroundTextt] = useState(
+    KEYCAP_TEXTURES[0].name,
+  );
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  function handleTextureSelect(texture: keycapTexture) {
+    if (texture.id === selectedTextureId || isAnimating) return;
+
+    setSelectedTextureId(texture.id);
+  }
+
+  const handleAnimationComplate = useCallback(() => {
+    setIsAnimating(false);
+  }, []);
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className='relative flex h-[90vh] min-h-250 flex-col overflow-hidden bg-linear-to-br from-[#0f172a] to-[#062f4a] text-white'
     >
-      Placeholder component for {slice.slice_type} (variation: {slice.variation}
-      ) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
+      <Canvas
+        camera={{ position: [0, 0.5, 0.5], fov: 45, zoom: 1.5 }}
+        className='-mb-[10vh] grow'
+      >
+        <Scene
+          selectedTextureId={selectedTextureId}
+          onAnimationComplate={handleAnimationComplate}
+        />
+      </Canvas>
+
+      <Bounded
+        className='relative shrink-0'
+        innerClassName='gap-6 lg:gap-8 flex flex-col lg:flex-row'
+      >
+        <div className='max-w-md shrink-0'>
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading2: ({ children }) => (
+                <h2 className='font-bold-slanted mb-1 text-4xl uppercase lg:mb-2 lg:text-6xl '>
+                  {children}
+                </h2>
+              ),
+            }}
+          />
+          <div className='text-pretty lg:text-lg'>
+            <PrismicRichText field={slice.primary.description} />
+          </div>
+        </div>
+
+        <ul className='grid grow grid-cols-2 gap-3 rounded-2xl bg-white p-4 text-black shadow-lg sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-3 xl:grid-cols-6'>
+          {KEYCAP_TEXTURES.map((textures) => (
+            <li key={textures.id}>
+              <button
+                onClick={() => handleTextureSelect(textures)}
+                className={clsx(
+                  'flex aspect-square flex-col items-center justify-center rounded-lg border-2 p-4 hover:scale-105 motion-safe:duration-300',
+                  selectedTextureId === textures.id
+                    ? 'border-[#81BFED] bg-[#81Bfed]/20'
+                    : 'cursor-pointer border-gray-300 hover:border-gray-500',
+                  isAnimating && 'cursor-not-allowed opacity-50',
+                )}
+              >
+                <div className='mb-3 overflow-hidden border-2 rounded border-black bg-gray-100'>
+                  <Image
+                    src={textures.path}
+                    alt={textures.name}
+                    width={400}
+                    height={255}
+                    className='h-full w-full object-cover'
+                  />
+                </div>
+                <span className='text-center text-sm font-semibold text-black'>
+                  {textures.name}
+                </span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </Bounded>
     </section>
   );
 };
