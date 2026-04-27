56a543607ccfe6a131dc5fb399c31181824a42f8
GiOrGI175 <161075967+GiOrGI175@users.noreply.github.com>
2026-04-26 22:08:08 -0700

    feature/animation in hero

--- slices/BentoBox/index.tsx
+++ slices/BentoBox/index.tsx
@@ -3,6 +3,8 @@ import { asText, Content } from '@prismicio/client';
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
 import { PrismicNextImage } from '@prismicio/next';
 import BentoBoxItem from '@/components/BentoBoxItem';
+import { Bounded } from '@/components/Bounded';
+import FadeIn from '@/components/FadeIn';
 
 /**
  * Props for `BentoBox`.
@@ -14,27 +16,28 @@ export type BentoBoxProps = SliceComponentProps<Content.BentoBoxSlice>;
  */
 const BentoBox: FC<BentoBoxProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <PrismicRichText
-        field={slice.primary.heading}
-        components={{
-          heading2: ({ children }) => (
-            <h2 className='font-bold-slanted mb-8 scroll-pt-6 text-6xl uppercase md:text-8xl'>
-              {children}
-            </h2>
-          ),
-        }}
-      />
-
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-6'>
+      <FadeIn>
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={{
+            heading2: ({ children }) => (
+              <h2 className='font-bold-slanted mb-8 scroll-pt-6 text-6xl uppercase md:text-8xl'>
+                {children}
+              </h2>
+            ),
+          }}
+        />
+      </FadeIn>
+      <FadeIn targetChildren className='grid grid-cols-1 gap-4 md:grid-cols-6'>
         {slice.primary.items.map((item) => (
           <BentoBoxItem key={asText(item.text)} item={item} />
         ))}
-      </div>
-    </section>
+      </FadeIn>
+    </Bounded>
   );
 };
 
--- slices/Hero/Scene.tsx
+++ slices/Hero/Scene.tsx
@@ -3,7 +3,14 @@
 import { Keyboard } from '@/components/KeyBoard';
 import { Keycap } from '@/components/Keycap';
 import { Environment, PerspectiveCamera } from '@react-three/drei';
-import { useControls } from 'leva';
+import { useRef, useState } from 'react';
+import * as THREE from 'three';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import { useControls } from 'leva';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export function Scene() {
   // const { positionX, positionY, positionZ, rotationX, rotationY, rotationZ } =
@@ -16,19 +23,84 @@ export function Scene() {
   //     rotationZ: 0,
   //   });
 
+  const KeyboardGroupRef = useRef<THREE.Group>(null);
+  const [lightIntensityScaler, setLightIntensityScaler] = useState(0);
+
   const scaleFactory = window.innerWidth <= 500 ? 0.5 : 1;
+
+  useGSAP(() => {
+    const mm = gsap.matchMedia();
+
+    mm.add('(prefers-reduced-motion: no-preference', () => {
+      if (!KeyboardGroupRef.current) return;
+
+      const keyboard = KeyboardGroupRef.current;
+
+      gsap.to(
+        { lightIntensityScaler: 0 },
+        {
+          lightIntensityScaler: 1,
+          duration: 3.5,
+          delay: 0.5,
+          ease: 'power2.inOut',
+          onUpdate: function () {
+            setLightIntensityScaler(this.targets()[0].lightIntensityScaler);
+          },
+        },
+      );
+
+      const tl = gsap.timeline({
+        ease: 'power2.inOut',
+      });
+
+      tl.to(keyboard.position, {
+        x: 0,
+        y: -0.5,
+        z: 0.5,
+        duration: 2,
+      })
+        .to(
+          keyboard.rotation,
+          {
+            x: 1.4,
+            y: 0,
+            z: 0,
+            duration: 1.8,
+          },
+          '<',
+        )
+        .to(keyboard.position, {
+          x: 0.2,
+          y: -0.5,
+          z: 1.9,
+          duration: 2,
+          delay: 0.5,
+        })
+        .to(
+          keyboard.rotation,
+          {
+            x: 1.6,
+            y: 0.4,
+            z: 0,
+            duration: 2,
+          },
+          '<',
+        );
+    });
+  });
 
   return (
     <group>
       <PerspectiveCamera makeDefault position={[0, 0, 4]} fov={50} />
 
       <group scale={scaleFactory}>
-        <Keyboard
-          scale={9}
-          position={[0.2, -0.5, 1.8]}
-          rotation={[1.6, 0.4, 0]}
-        />
-
+        <group
+          ref={KeyboardGroupRef}
+          // position={[0.2, -0.5, 1.8]}
+          // rotation={[1.6, 0.4, 0]}
+        >
+          <Keyboard scale={9} />
+        </group>
         <group>
           <Keycap position={[0, -0.4, 2.6]} texture={0} />
           <Keycap position={[-1.4, 0, 2.3]} texture={1} />
@@ -45,12 +117,12 @@ export function Scene() {
 
       <Environment
         files={['/hdr/blue-studio.hdr']}
-        environmentIntensity={0.05}
+        environmentIntensity={0.2 * lightIntensityScaler}
       />
 
       <spotLight
         position={[-2, 1.5, 3]}
-        intensity={30}
+        intensity={30 * lightIntensityScaler}
         castShadow
         shadow-bios={-0.0002}
         shadow-normalBios={0.002}
