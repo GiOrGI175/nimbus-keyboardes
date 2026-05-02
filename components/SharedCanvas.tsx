f4e201af6d377b404bd6599444b7cf09b5fbbf66
GiOrGI175 <161075967+GiOrGI175@users.noreply.github.com>
2026-05-01 17:45:46 -0700

    feature/switch playground

--- components/SharedCanvas.tsx
+++ components/SharedCanvas.tsx
@@ -1,5 +1,12 @@
+'use client';
+
 import { Content, isFilled } from '@prismicio/client';
+import { Canvas } from '@react-three/fiber';
 import clsx from 'clsx';
+import { SOUND_MAP, Switch } from './Switch';
+import { Stage } from '@react-three/drei';
+import gsap from 'gsap';
+import { LuVolume2 } from 'react-icons/lu';
 
 type SharedCanvasProps = {
   color: Content.SwitchPlaygroundSliceDefaultPrimarySwitchesItem['switch'];
@@ -19,8 +26,36 @@ export default function SharedCanvas({ color }: SharedCanvasProps) {
     black: 'bg-gray-900',
   }[colorName];
 
+  const handleSound = () => {
+    const selectedSound = gsap.utils.random(SOUND_MAP[colorName]);
+
+    const audio = new Audio(selectedSound);
+    audio.volume = 0.6;
+    audio.play();
+  };
+
   return (
     <div className='group relative min-h-96 overflow-hidden rounded-3xl select-none'>
+      <button
+        onClick={handleSound}
+        className='font-bold-slanted absolute bottom-0 left-0 z-10 flex items-center gap-3 p-6 text-4xl text-white uppercase focus:ring-2 focus:ring-white focus:outline-none'
+      >
+        {name} <LuVolume2 />
+      </button>
+      <Canvas camera={{ position: [1.5, 2, 0], fov: 7 }}>
+        <Stage
+          adjustCamera
+          intensity={0.5}
+          shadows={'contact'}
+          environment='city'
+        >
+          <Switch
+            rotation={[0, Math.PI / 4, 0]}
+            color={colorName}
+            hexColor={hexColor || ''}
+          />
+        </Stage>
+      </Canvas>
       <div
         className={clsx(
           'font-black-slanted absolute inset-0 -z-10 grid place-items-center uppercase text-8xl',
@@ -32,6 +67,7 @@ export default function SharedCanvas({ color }: SharedCanvasProps) {
             x='50%'
             y='50%'
             dominantBaseline='middle'
+            textAnchor='middle'
             fontSize={18}
             className='font-black-slanted fill-white/30 uppercase mix-blend-overlay group-hover:fill-white motion-safe:transition-all motion-safe:duration-700'
           >
